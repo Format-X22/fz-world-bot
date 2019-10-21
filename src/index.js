e6ed56b04b5e2eacfc57a4c0eeec9297c610fdd8
Oleg <oleg.pav.m@gmail.com>
2019-10-21 19:10:43 +0300

    Draft.

--- src/index.js
+++ src/index.js
@@ -15,21 +15,50 @@ app.get('/search.html', search);
 app.listen(port, () => console.log(`On port ${port}!`));
 
 function profile(req, res) {
-    res.send(pug.renderFile(`${uiPath}profile.html`, {}));
+    res.send(
+        pug.renderFile(`${uiPath}profile.pug`, {
+            name: 'Steve Jobs',
+            nick: '@steveJobs',
+            description: 'Играю в ФЗ 10 лет, люблю продукцию Apple!',
+        })
+    );
 }
 
 function editProfile(req, res) {
-    res.send(pug.renderFile(`${uiPath}editProfile.html`, {}));
+    res.send(
+        pug.renderFile(page('editProfile'), {
+            description: 'Играю в ФЗ 10 лет, люблю продукцию Apple!',
+        })
+    );
 }
 
 function join(req, res) {
-    res.send(pug.renderFile(`${uiPath}join.html`, {}));
+    res.send(pug.renderFile(page('join'), {}));
 }
 
 function search(req, res) {
-    res.send(pug.renderFile(`${uiPath}search.html`, {}));
+    res.send(pug.renderFile(page('search'), {}));
 }
 
 function unregistered(req, res) {
-    res.send(pug.renderFile(`${uiPath}unregistered.html`, {}));
+    res.send(pug.renderFile(page('unregistered'), {}));
 }
+
+function page(name) {
+    return `${uiPath}${name}.pug`;
+}
+
+const TelegramBot = require('node-telegram-bot-api');
+
+const bot = new TelegramBot(process.env.FZ_BOT_KEY, { polling: true });
+
+bot.on('message', msg => {
+    const chatId = msg.chat.id;
+
+    // send a message to the chat acknowledging receipt of their message
+    bot.sendGame(chatId, 'fzWorldBot');
+});
+
+bot.on('callback_query', function onCallbackQuery(callbackQuery) {
+    bot.answerCallbackQuery(callbackQuery.id, { url: 'https://www.google.com/?client=safari' });
+});
