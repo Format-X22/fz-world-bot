d04cd0234616507d51279debb7550745c2a3a537
Oleg <oleg.pav.m@gmail.com>
2019-10-30 15:02:18 +0300

    Fixes.

--- src/Main.js
+++ src/Main.js
@@ -85,15 +85,18 @@ class Main {
             if (user) {
                 if (!fs.existsSync(__dirname + '/static/avatar/' + user.username + '.jpeg')) {
                     const res = await bot.getUserProfilePhotos(msg.from.id);
-                    const file = await bot.getFile(res.photos[0][1].file_id);
-                    const path = file.file_path;
-                    const key = process.env.FZ_BOT_KEY;
-                    const data = await fetch(`https://api.telegram.org/file/bot${key}/${path}`);
-                    const to = fs.createWriteStream(
-                        __dirname + '/static/avatar/' + user.username + '.jpeg'
-                    );
 
-                    data.body.pipe(to);
+                    if (res.photos.length) {
+                        const file = await bot.getFile(res.photos[0][1].file_id);
+                        const path = file.file_path;
+                        const key = process.env.FZ_BOT_KEY;
+                        const data = await fetch(`https://api.telegram.org/file/bot${key}/${path}`);
+                        const to = fs.createWriteStream(
+                            __dirname + '/static/avatar/' + 'x' + '.jpeg'
+                        );
+
+                        data.body.pipe(to);
+                    }
                 }
 
                 await global.db
@@ -134,6 +137,7 @@ class Main {
             ].includes(req.path)
         ) {
             res.redirect('unregistered');
+            return;
         }
 
         next();
