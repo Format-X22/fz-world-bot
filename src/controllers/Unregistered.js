863fd3195e3032c1220b65818f23d41698aca1c0
Oleg <oleg.pav.m@gmail.com>
2020-02-18 18:24:52 +0300

    Fix undefined user.

--- src/controllers/Unregistered.js
+++ src/controllers/Unregistered.js
@@ -2,7 +2,7 @@ const Abstract = require('./Abstract');
 
 class Unregistered extends Abstract {
     async getPage(req, res) {
-        console.log('UNREGISTERED', req.user.username);
+        console.log('UNREGISTERED', req.user);
         res.send(this.renderPage('unregistered', { user: req.user }));
     }
 }
